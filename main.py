061e1a822a19620c9c78396410e54c3786f85e6e
Vitor <vitorpgmdr@gmail.com>
2021-09-13 01:55:05 -0300

    alterar paths para instancias de pathlib.Path
    Faz as alteraçoes e adequa o programa

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import os
-from datetime import datetime
 import shutil
 import subprocess
+from datetime import datetime
+from pathlib import Path
 from urllib.parse import unquote
+
 from PIL import Image
 
-
-BASE_GOOGLE_DIR = '/home/vitor/.config/google-chrome/Default/'
-LOG_FILE_NAME = '/home/vitor/my-daemons/my-logs.log'
+GOOGLE_BACKGROUND_FILE_PATH = Path(
+    '/home/vitor/.config/google-chrome/Default/background.jpg')
+LOG_FILE_NAME = Path('/home/vitor/my-daemons/my-logs.log')
 
 
 def log_error(msg):
@@ -18,30 +19,20 @@ def log_error(msg):
         )
 
 
-def get_background_files_path():
+def get_background_file_path():
     background_image_dconf_path = subprocess.run(
         ['gsettings', 'get', 'org.gnome.desktop.background', 'picture-uri'],
         capture_output=True,
         text=True
     ).stdout
 
-    background_image_path = unquote(background_image_dconf_path[8:-2])
-    _, file_ext = os.path.splitext(background_image_path)
-
-    new_background_image_path = os.path.join(
-        BASE_GOOGLE_DIR, f'background{file_ext}')
-
-    return background_image_path, new_background_image_path
+    return unquote(background_image_dconf_path[8:-2])
 
 
 def convert_background_to_jpg(background_path):
-    file_name, _ = os.path.splitext(background_path)
-    # O arquivo deve estar no formato .jpg
-    new_background_path = file_name + '.jpg'
-
     with Image.open(background_path) as img:
         # Converte para RGB pois é preciso para a conversão para JPEG
-        img.convert('RGB').save(new_background_path, 'JPEG')
+        img.convert('RGB').save(background_path, 'JPEG')
 
 
 def save_log(background_image_path, new_background_image_path):
@@ -55,15 +46,15 @@ def save_log(background_image_path, new_background_image_path):
 
 
 def main():
-    background_image_path, new_background_image_path = get_background_files_path()
+    background_image_path = get_background_file_path()
 
     try:
-        shutil.copy(background_image_path, new_background_image_path)
-        convert_background_to_jpg(new_background_image_path)
+        shutil.copy(background_image_path, GOOGLE_BACKGROUND_FILE_PATH)
+        convert_background_to_jpg(GOOGLE_BACKGROUND_FILE_PATH)
     except Exception as e:
         log_error(e.args)
     else:
-        save_log(background_image_path, new_background_image_path)
+        save_log(background_image_path, GOOGLE_BACKGROUND_FILE_PATH)
 
 
 if __name__ == '__main__':
